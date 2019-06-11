e8cba87b27a609ca5576447e93ce7fffbf750f3b
Kevin Kim <kevinkim.2012@gmail.com>
2019-06-10 20:53:14 -0700

    redux persist works now need to add thunk and redux store strengthprofile

--- workout-app-dotnet/ClientApp/src/components/store.js
+++ workout-app-dotnet/ClientApp/src/components/store.js
@@ -1,5 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
-import { strengthProfileReducer } from "./actions";
+import { createStore } from "redux";
 import { persistReducer, persistStore } from "redux-persist";
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import storage from "redux-persist/lib/storage";
@@ -13,30 +12,8 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-// const middleware = compose(
-//   applyMiddleware(
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// );
-
 export const store = createStore(
   persistedReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 export const persistor = persistStore(store);
-
-// const persist = (strengthProfileConfig, reducer) =>
-//   persistReducer({ ...strengthProfileConfig, storage }, reducer);
-
-// const reducers = combineReducers({
-//   persistedStore: persist(strengthProfileConfig, strengthProfileReducer)
-// });
-
-// const store = createStore(
-//   reducers,
-//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-// );
-
-// export const persistor = persistStore(store);
-
-// export default store;
