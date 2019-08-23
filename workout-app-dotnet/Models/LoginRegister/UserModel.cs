dce78271092318e1501e11eb4f684e577ce5ea25
kevdev81 <kevdev81@users.noreply.github.com>
2019-08-23 16:51:14 -0700

    cleaning classes in .net

--- workout-app-dotnet/Models/LoginRegister/UserModel.cs
+++ workout-app-dotnet/Models/LoginRegister/UserModel.cs
@@ -5,7 +5,6 @@ namespace workoutApp.Models.LoginRegister
     public class UserModel
     {
         public int Id { get; set; }
-        public int UserRoledId { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public bool IsConfirmed { get; set; }
